55e6856e8ca05972358e92c8ec2cb1b416f040e1
Shubham Kharat <87020265+Technocratshubham@users.noreply.github.com>
2023-06-28 15:05:08 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,27 @@
+function goToEmergencyFund() {
+    window.location.href = "rule1.html";
+}
+
+function goToRule2() {
+    window.location.href = "rule2.html";
+}
+
+function goToRule3() {
+    window.location.href = "rule3.html";
+}
+
+function goToRule4() {
+    window.location.href = "rule4.html";
+}
+
+function goToRule5() {
+    window.location.href = "rule5.html";
+}
+
+function goToRule6() {
+    window.location.href = "rule6.html";
+}
+
+function goToRule7() {
+    window.location.href = "rule7.html";
+}
